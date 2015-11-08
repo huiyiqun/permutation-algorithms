9f251f2d80c85767e6c0132517d8aad135c53101
惠轶群 <huiyiqun@gmail.com>
2015-11-08 23:54:02 +0800

    iterative implementation to generate dict order sequence

--- dict.py
+++ dict.py
@@ -5,6 +5,8 @@
 #
 # Under here is two implementation recursive and iterative
 ###
+import math
+from common import integer_to_ascending_system_number
 
 
 def recursive_PGA_with_dict(width):
@@ -39,8 +41,37 @@ def recursive_PGA_with_dict(width):
     return _recursive_PGA_with_dict(list(range(width)))
 
 
+def iterative_PGA_with_dict(width):
+    '''
+    Iterative permutation generation algorithm according
+    to dict order.
+    '''
+
+    total_numbers = math.factorial(width)
+    for i in range(total_numbers):
+        asc_number = integer_to_ascending_system_number(i)
+        asc_number += (width - len(asc_number)) * [0]  # align
+        ret = []
+        ordered_index = list(range(width))
+        for n in reversed(asc_number):
+            ret.append(ordered_index[n])
+            del ordered_index[n]
+        yield list(ret)
+
+
 if __name__ == '__main__':
+    # Verify results
+    for i_result, r_result in zip(iterative_PGA_with_dict(9), recursive_PGA_with_dict(9)):
+        if i_result != r_result:
+            # print(i_result, r_result)
+            print('Failed!')
+
     # For profiler
-    # python -m cProfile switch.py
-    for r_result in recursive_PGA_with_dict(9):
-        print(r_result)
+    # python -m cProfile dict.py
+    #for r_result in recursive_PGA_with_dict(9):
+    #   print(r_result)
+
+    # For profiler
+    # python -m cProfile dict.py
+    #for i_result in iterative_PGA_with_dict(9):
+    #    print(i_result)
