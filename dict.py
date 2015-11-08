fbe73eb8b11764e75a95e64d19b862a93cd93b8c
惠轶群 <huiyiqun@gmail.com>
2015-11-08 23:02:37 +0800

    arrange code

--- dict.py
+++ dict.py
@@ -7,30 +7,31 @@
 ###
 
 
-def _recursive_PGA_with_dict(sequence):
-    '''
-    Note:
-        `sequence` must be dict-ordered.
-    '''
-    # boundary
-    if not len(sequence):
-        # Empty
-        yield sequence
-        return
-
-    # recursion
-    for i, first_index in enumerate(sequence):
-        seq_copy = sequence.copy()
-        del seq_copy[i]
-        for sub_permutation in _recursive_PGA_with_dict(seq_copy):
-            yield [first_index, ] + sub_permutation
-
-
 def recursive_PGA_with_dict(width):
     '''
     Recursive permutation generation algorithm according
     to dict order.
     '''
+
+    def _recursive_PGA_with_dict(sequence):
+        '''
+        Note:
+            `sequence` must be dict-ordered.
+        '''
+        # boundary
+        if not len(sequence):
+            # Empty
+            yield sequence
+            return
+
+        # recursion
+        for i, first_index in enumerate(sequence):
+            seq_copy = sequence.copy()
+            del seq_copy[i]
+            for sub_permutation in _recursive_PGA_with_dict(seq_copy):
+                yield [first_index, ] + sub_permutation
+
+
     # condition
     assert width >= 0
     assert type(width) is int
