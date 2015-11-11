03d0942247760d2f3bb6b20a14be1fed8b0487ac
惠轶群 <huiyiqun@gmail.com>
2015-11-11 21:06:12 +0800

    integer to descending number

--- common.py
+++ common.py
@@ -1,6 +1,7 @@
 ###
 # Common functions to be used by other script
 ###
+import math
 
 def integer_to_ascending_system_number(integer):
     '''
@@ -17,6 +18,23 @@ def integer_to_ascending_system_number(integer):
             system += 1
     return list(_itasn(integer))
 
+def integer_to_descending_system_number(integer, width):
+    '''
+    covert number integer to this kind of number,
+    whose system is ascending.
+    '''
+    assert type(integer) is int
+    assert type(width) is int
+    assert integer < math.factorial(width)
+
+    def _itasn(integer):
+        system = width
+        while integer:
+            yield integer % system
+            integer //= system
+            system -= 1
+    return list(_itasn(integer))
+
 if __name__ == '__main__':
     for i in range(24):
-        print(integer_to_ascending_system_number(i))
+        print(integer_to_descending_system_number(i, 4))
